5e6812ccc35feef31ff40c051975287094a9ca75
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-19 12:58:31 +0100

    Adds constants

---
+++ src/js/api/constants.js
@@ -0,0 +1,11 @@
+export const API_KEY = "a1fec8b5-7968-416b-ade4-76f317b0eb39";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH_LOGIN = `${API_BASE}/auth/login`;
+
+export const API_AUTH_REGISTER = `${API_BASE}/auth/register`;
+
+export const API_AUCTION_LISTINGS = `${API_BASE}/auction/listings`;
+
+export const API_AUCTION_PROFILES = `${API_BASE}/auction/PROFILES`;
