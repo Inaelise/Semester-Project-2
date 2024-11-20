a21fcbbe369e1970b8d8b23f77df856cbef889b0
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-20 16:34:57 +0100

    Changes to file name

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -9,14 +9,14 @@ export default async function router(pathname = window.location.pathname) {
     case "/auth/register/":
       await import("./views/register.js");
       break;
-    case "/post/":
-      await import("./views/post.js");
+    case "/listing/":
+      await import("./views/listing.js");
       break;
-    case "/post/edit/":
-      await import("./views/postEdit.js");
+    case "/listing/edit/":
+      await import("./views/listingEdit.js");
       break;
-    case "/post/create/":
-      await import("./views/postCreate.js");
+    case "/listing/create/":
+      await import("./views/listingCreate.js");
       break;
     case "/profile/":
       await import("./views/profile.js");
