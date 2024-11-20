571952d93a9393ef357297a9fd5d9fce97f0ba7f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-20 09:57:08 +0100

    Adds login function and form handler

---
+++ src/js/api/auth/login.js
@@ -0,0 +1,24 @@
+import { headers } from "../headers";
+import { save } from "../../storage/save";
+import { API_AUTH_LOGIN } from "../constants";
+
+export async function login({ email, password }) {
+  const response = await fetch(API_AUTH_LOGIN, {
+    headers: headers("application/json"),
+    method: "post",
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    const { data } = result;
+    console.log(data);
+    save("token", data.accessToken);
+    save("user", data.name);
+    return data;
+  }
+
+  const errorMessage = result.errors.map((error) => error.message).join("\r\n");
+
+  throw new Error(errorMessage);
+}
---
+++ src/js/ui/auth/login.js
@@ -0,0 +1,19 @@
+import { login } from "../../api/auth/login";
+
+export async function onLogin(e) {
+  e.preventDefault();
+
+  const loginForm = e.target;
+  const formData = new FormData(loginForm);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    await login(data);
+    // Temporary alert
+    alert("You're logged in");
+    /* window.location.href = "/"; */
+  } catch (error) {
+    // Temporary error alert
+    alert(error);
+  }
+}
