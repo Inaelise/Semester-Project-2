221ea1739a385ae817c94fc89c33c14afcd9d9c4
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-20 16:35:22 +0100

    Adds api key

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,8 +1,13 @@
+import { API_KEY } from "./constants";
 import { load } from "../storage/load";
 
 export const headers = (contentType) => {
   const token = load("token");
   const headers = {};
+
+  if (API_KEY) {
+    headers["X-Noroff-API-KEY"] = API_KEY;
+  }
 
   if (contentType) {
     headers["Content-Type"] = contentType;
