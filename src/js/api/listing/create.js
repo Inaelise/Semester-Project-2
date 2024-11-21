5b91a7c280bb3e5765f5182b91f495bd983391c4
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-21 14:22:08 +0100

    Adds create listing function and handler

---
+++ src/js/api/listing/create.js
@@ -0,0 +1,29 @@
+import { API_AUCTION_LISTINGS } from "../constants";
+import { headers } from "../headers";
+
+export async function createListing({
+  title,
+  description,
+  tags,
+  media,
+  endsAt,
+}) {
+  const response = await fetch(API_AUCTION_LISTINGS, {
+    headers: headers("application/json"),
+    method: "post",
+    body: JSON.stringify({ title, description, tags, media, endsAt }),
+  });
+
+  const result = await response.json();
+
+  if (response.ok) {
+    const { data } = result;
+    //Temporary alert
+    alert("Post created!");
+    console.log(data);
+    return data;
+  }
+  const errorMessage = result.errors.map((error) => error.message).join("\r\n");
+
+  throw new Error(errorMessage);
+}
