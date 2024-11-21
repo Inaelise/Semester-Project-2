d0ccc5a289250bf9ff374092337a997720cda7db
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-21 22:31:52 +0100

    Adds end time text and bid history elements

--- src/js/ui/listing/viewListing.js
+++ src/js/ui/listing/viewListing.js
@@ -89,12 +89,52 @@ export async function viewListing() {
 
     const endTimeContainer = document.createElement("div");
     createCountdown(listing.endsAt, endTimeContainer);
+    const endTimeText = document.createElement("p");
+    endTimeText.textContent = "Ends in";
+    endTimeContainer.append(endTimeText);
 
     const description = document.createElement("p");
     description.textContent = listing.description;
 
     const tags = document.createElement("p");
-    tags.textContent = listing.tags;
+    tags.textContent = listing.tags.join(", ");
+
+    const bidHistoryContainer = document.createElement("div");
+    const bidsTitle = document.createElement("h3");
+    bidsTitle.textContent = "Bid history";
+    bidHistoryContainer.append(bidsTitle);
+
+    if (listing.bids.length > 0) {
+      const sortedBids = [...listing.bids].sort((a, b) => b.amount - a.amount);
+      sortedBids.forEach((bid) => {
+        const bidContainer = document.createElement("div");
+
+        const bidderInfo = document.createElement("div");
+        const bidderAvatar = document.createElement("img");
+        bidderAvatar.src = bid.bidder.avatar.url;
+
+        const bidderName = document.createElement("p");
+        bidderName.textContent = bid.bidder.name;
+
+        bidderInfo.append(bidderAvatar, bidderName);
+
+        const bidAmountDiv = document.createElement("div");
+        const bidAmount = document.createElement("p");
+        bidAmount.textContent = bid.amount;
+
+        const creditIcon = document.createElement("span");
+        creditIcon.innerHTML = "<i class='fa-solid fa-coins'></i>";
+
+        bidAmountDiv.append(bidAmount, creditIcon);
+
+        bidContainer.append(bidderInfo, bidAmountDiv);
+        bidHistoryContainer.append(bidContainer);
+      });
+    } else {
+      const noBids = document.createElement("p");
+      noBids.textContent = "No bids have been made yet";
+      bidHistoryContainer.append(noBids);
+    }
 
     divThree.append(endTimeContainer);
     divTwo.append(highestBidContainer);
@@ -103,7 +143,7 @@ export async function viewListing() {
     listingInfo.insertBefore(divTwo, form);
     listingInfo.insertBefore(divThree, form);
     listingInfo.insertBefore(description, form);
-    listingInfo.append(tags);
+    listingInfo.append(tags, bidHistoryContainer);
 
     return listingContainer;
   } catch (error) {
