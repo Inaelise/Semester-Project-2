2fd5614d4c131f97da19713ef44f56c50fcb2304
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-21 21:22:41 +0100

    Adds 1000 credits to users when registering

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -5,7 +5,7 @@ export async function register({ name, email, password }) {
   const response = await fetch(API_AUTH_REGISTER, {
     headers: headers("application/json"),
     method: "post",
-    body: JSON.stringify({ name, email, password }),
+    body: JSON.stringify({ name, email, password, credits: 1000 }),
   });
 
   if (response.ok) {
