92e4acbf97a4ceee3dc246fdbdf3b4bd082d293d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-22 12:12:55 +0100

    Adds populate function

--- src/js/router/views/listingEdit.js
+++ src/js/router/views/listingEdit.js
@@ -1,12 +1,14 @@
 import { onDelete } from "../../ui/listing/delete";
 import { onUpdateListing } from "../../ui/listing/update";
 import { authGuard } from "../../utilities/authGuard";
+import { populateListingEdit } from "../../utilities/populateForm";
 
 authGuard();
 
 const form = document.forms.editListing;
 
 form.addEventListener("submit", onUpdateListing);
+populateListingEdit();
 
 const deleteBtn = document.getElementById("delete-btn");
 if (deleteBtn) {
