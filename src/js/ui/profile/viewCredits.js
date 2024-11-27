b6e75005d2d12a0f73880a994775bbc8b5034c66
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-27 13:20:22 +0100

    Changes to constant

--- src/js/ui/profile/viewCredits.js
+++ src/js/ui/profile/viewCredits.js
@@ -3,13 +3,13 @@ import { activeUser } from "../../utilities/activeUser";
 import { displayMessage } from "../../utilities/displayMessage";
 
 export async function displayCredits() {
-  const creditContainer = document.getElementById("credits");
+  const credits = document.getElementById("credits");
   const user = activeUser();
 
   try {
     const amount = await getCredits(user);
-    if (creditContainer) {
-      creditContainer.textContent = amount.credits;
+    if (credits) {
+      credits.textContent = amount.credits;
     }
   } catch (error) {
     displayMessage("credits", error.message);
