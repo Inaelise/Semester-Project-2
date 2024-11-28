8a67c540bb94eee7d7db1d1e994d1c5c28f6c164
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-28 14:09:39 +0100

    Updated function to handle multiple images and replaced alert with displayMessage

--- src/js/utilities/populateForm.js
+++ src/js/utilities/populateForm.js
@@ -2,6 +2,7 @@ import { activeUser } from "./activeUser";
 import { activeListingId } from "./activeListingId";
 import { getProfile } from "../api/profile/read";
 import { getListing } from "../api/listing/read";
+import { displayMessage } from "./displayMessage";
 
 export async function populateListingEdit() {
   const listingId = activeListingId();
@@ -13,15 +14,45 @@ export async function populateListingEdit() {
 
     form["title"].value = listing.title || "";
     form["description"].value = listing.description || "";
-
-    const firstMediaUrl =
-      listing.media && listing.media.length > 0 ? listing.media[0].url : "";
-
-    form["image"].value = firstMediaUrl;
     form["tags"].value = listing.tags ? listing.tags.join(", ") : "";
+
+    const editImg = document.getElementById("edit-img");
+    editImg.innerHTML = "";
+
+    if (listing.media && listing.media.length > 0) {
+      listing.media.forEach((img) => {
+        const imgInput = document.createElement("div");
+        imgInput.className = "img-input";
+        imgInput.innerHTML = `<input
+            type="url"
+            name="images[]"
+            placeholder="https://exampleurl.com"
+            pattern="https://.*"
+            value="${img.url}"
+          />
+          <button type="button" class="img-remove" title="Click to remove image URL">
+            <i class="fa-solid fa-trash"></i>
+          </button>`;
+
+        editImg.append(imgInput);
+      });
+    } else {
+      const imgInput = document.createElement("div");
+      imgInput.className = "img-input";
+      imgInput.innerHTML = `<input
+            type="url"
+            name="images[]"
+            placeholder="https://exampleurl.com"
+            pattern="https://.*"
+          />
+          <button type="button" class="img-remove" title="Click to remove image URL">
+            <i class="fa-solid fa-trash"></i>
+          </button>`;
+
+      editImg.append(imgInput);
+    }
   } catch (error) {
-    //temporary error alert
-    alert("Error loading post data to input fields.");
+    displayMessage("message", "Error loading input field data.");
   }
 }
 
@@ -36,7 +67,6 @@ export async function populateProfileEdit() {
     form["avatar"].value = profile.avatar?.url || "";
     form["bio"].value = profile.bio || "";
   } catch (error) {
-    //temporary error alert
-    alert("Error loading profile data to input fields.");
+    displayMessage("message", "Error loading input field data.");
   }
 }
