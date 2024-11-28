a73fa43e2294b0ae15c1d2a03676eab42ebd3a49
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-28 14:13:33 +0100

    Adds event listeners for dynamic image inputs

--- src/js/router/views/listingCreate.js
+++ src/js/router/views/listingCreate.js
@@ -5,4 +5,34 @@ authGuard();
 
 const form = document.forms.createListing;
 
+const addImgBtn = document.getElementById("add-img");
+const imgContainer = document.getElementById("img-container");
+
+addImgBtn.addEventListener("click", () => {
+  const newImg = document.createElement("div");
+  newImg.className = "img-input";
+  newImg.innerHTML = `
+    <input
+      type="url"
+      id="image"
+      name="images[]"
+      placeholder="https://exampleurl.com"
+      pattern="https://.*"
+    />
+    <button type="button" class="img-remove" title="Click to remove image url"><i class="fa-solid fa-trash"></i></button>
+  `;
+
+  imgContainer.append(newImg);
+});
+
+imgContainer.addEventListener("click", (e) => {
+  /* if (e.target.classList.contains("img-remove")) {
+    console.log("Remove button clicked");
+    e.target.closest(".image-input").remove();
+  } */
+  if (e.target.closest(".img-remove")) {
+    e.target.closest(".img-input").remove();
+  }
+});
+
 form.addEventListener("submit", onCreate);
--- src/js/router/views/listingEdit.js
+++ src/js/router/views/listingEdit.js
@@ -7,6 +7,32 @@ authGuard();
 
 const form = document.forms.editListing;
 
+const addImgBtn = document.getElementById("add-img");
+const imgContainer = document.getElementById("edit-img");
+
+addImgBtn.addEventListener("click", () => {
+  const newImg = document.createElement("div");
+  newImg.className = "img-input";
+  newImg.innerHTML = `
+    <input
+      type="url"
+      id="image"
+      name="images[]"
+      placeholder="https://exampleurl.com"
+      pattern="https://.*"
+    />
+    <button type="button" class="img-remove" title="Click to remove image url"><i class="fa-solid fa-trash"></i></button>
+  `;
+
+  imgContainer.append(newImg);
+});
+
+imgContainer.addEventListener("click", (e) => {
+  if (e.target.closest(".img-remove")) {
+    e.target.closest(".img-input").remove();
+  }
+});
+
 form.addEventListener("submit", onUpdateListing);
 populateListingEdit();
 
