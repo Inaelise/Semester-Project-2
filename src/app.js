e302e46b44669e184dd3998424715321b22ffcb8
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-02 16:23:12 +0100

    Adds function to toggle dropdown menu

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import "./css/style.css";
 
 import router from "./js/router";
 import { displayHeader } from "./js/utilities/displayHeader";
+import { toggleDropdown } from "./js/utilities/toggleDropdown";
 
 await router(window.location.pathname);
 
 displayHeader();
+toggleDropdown();
