9d3a98f44225e60d3277cfbd6931adb77f0bb3c3
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-02 13:33:18 +0100

    Change to select all logout btns

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,12 +1,14 @@
 import { logout } from "../auth/logout";
 
 export function onLogout() {
-  const logoutBtn = document.getElementById("logout-btn");
+  const logoutBtns = document.querySelectorAll(".logout-btn");
 
-  logoutBtn.addEventListener("click", () => {
-    if (window.confirm("Are you sure you want to logout?") === true) {
-      logout();
-      window.location.href = "/";
-    }
+  logoutBtns.forEach((btns) => {
+    btns.addEventListener("click", () => {
+      if (window.confirm("Are you sure you want to logout?") === true) {
+        logout();
+        window.location.href = "/";
+      }
+    });
   });
 }
