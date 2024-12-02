963210632105a75fb2b8fb8421b1a61094a7503b
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-02 13:31:35 +0100

    Adds themes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#41354D",
+        secondary: "#5B0088",
+        alt: "E0E0E0",
+      },
+      fontFamily: {
+        header: ["Inter", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        small: "14px",
+      },
+      backgroundImage: {
+        auth: "url('/images/waves-login-register.png')",
+        profile: "url('/images/waves-profile.png')",
+      },
+      dropShadow: {
+        btns: "0 4px 2px rgba(0, 0, 0, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
