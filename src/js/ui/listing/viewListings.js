f5e654234a37a593b79bcfc362b7aa0778b3d8f8
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-03 16:53:06 +0100

    Adds styling classes to h1, tag filter elements, credits, search, create button and list items

--- src/js/ui/listing/viewListings.js
+++ src/js/ui/listing/viewListings.js
@@ -38,6 +38,8 @@ export async function viewListings(limit = 9, page = 1, query = "", tag = "") {
     // Render listings
     listings.forEach((item) => {
       const listItem = document.createElement("li");
+      listItem.classList.add("w-[245px]", "hover");
+      listItem.title = "Go to listing";
 
       const link = document.createElement("a");
       link.href = `/listing/?id=${item.id}`;
@@ -45,9 +47,11 @@ export async function viewListings(limit = 9, page = 1, query = "", tag = "") {
       const itemContainer = document.createElement("div");
 
       const title = document.createElement("h2");
+      title.classList.add("font-medium", "pt-1", "pb-2");
       title.textContent = item.title || "No title available";
 
       const img = document.createElement("img");
+      img.classList.add("w-full", "h-[281px]", "object-cover");
       if (item.media?.length > 0) {
         const selectedImg = item.media[0];
         img.src = selectedImg.url || "/images/default-img.png";
@@ -58,20 +62,29 @@ export async function viewListings(limit = 9, page = 1, query = "", tag = "") {
       }
 
       const expireDiv = document.createElement("div");
+      expireDiv.classList.add("pb-3");
       const expireText = document.createElement("p");
+      expireText.classList.add("opacity-50");
       expireText.textContent = "Expires";
       expireDiv.append(expireText);
       createCountdown(item.endsAt, expireDiv);
 
       const bidDiv = document.createElement("div");
       const bidText = document.createElement("p");
+      bidText.classList.add("opacity-50");
       bidText.textContent = "Highest bid";
+      const bidContainer = document.createElement("div");
+      bidContainer.classList.add("flex", "items-center", "gap-2");
       const bid = document.createElement("p");
+      bid.classList.add("text-medium");
       bid.textContent =
         item.bids?.length > 0
           ? Math.max(...item.bids.map((bid) => bid.amount))
           : 0;
-      bidDiv.append(bidText, bid);
+      const creditIcon = document.createElement("span");
+      creditIcon.innerHTML = `<i class="fa-solid fa-coins fa-sm"></i>`;
+      bidContainer.append(bid, creditIcon);
+      bidDiv.append(bidText, bidContainer);
 
       itemContainer.append(img, title, expireDiv, bidDiv);
       link.append(itemContainer);
