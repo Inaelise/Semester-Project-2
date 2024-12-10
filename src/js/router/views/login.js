c1ab1a1d4856a86ce2641c30d2a11c785ad014cc
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-10 14:37:42 +0100

    Disables login button and displays message if user is already logged in

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,5 +1,20 @@
 import { onLogin } from "../../ui/auth/login";
+import { activeUser } from "../../utilities/activeUser";
+import { displayMessage } from "../../utilities/displayMessage";
 
 const form = document.forms.login;
+const loginBtn = document.getElementById("login-btn");
+const user = activeUser();
+
+if (user) {
+  displayMessage("message", "You're already logged in.", true);
+  loginBtn.disabled = true;
+  loginBtn.classList.add(
+    "disabled:bg-[#767676]",
+    "disabled:hover:scale-100",
+    "disabled:hover:shadow-none",
+    "disabled:drop-shadow-none"
+  );
+}
 
 form.addEventListener("submit", onLogin);
