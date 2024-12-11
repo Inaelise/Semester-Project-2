978f20469133487eef4e565222ed62f621b48fe8
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-11 20:20:20 +0100

    Changed structure of functions

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import "./css/style.css";
-
-import router from "./js/router";
 import { displayHeader } from "./js/utilities/displayHeader";
 import { toggleDropdown } from "./js/utilities/toggleDropdown";
-
-await router(window.location.pathname);
+import router from "./js/router";
 
 displayHeader();
 toggleDropdown();
+await router(window.location.pathname);
