39dbcffd55858be3b5690aa207d0367a4173c8be
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-12-11 19:24:54 +0100

    Adds rollup input options

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -6,7 +7,15 @@ export default defineConfig({
   build: {
     target: "esnext",
     rollupOptions: {
-      input: {},
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        listing: resolve(__dirname, "./listing/index.html"),
+        editListing: resolve(__dirname, "./listing/edit/index.html"),
+        createListing: resolve(__dirname, "./listing/create/index.html"),
+      },
     },
   },
 });
